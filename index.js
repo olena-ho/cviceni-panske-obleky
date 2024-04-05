40f70325fae8d30e9f45b36299c464db2acac692
Olena Horovenko <elena.gorovenko@gmail.com>
2024-04-05 16:58:22 +0200

    version with onclick change of colors and stroke

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-// Color of t-shirt
+function color(z) {
+  const a = getComputedStyle(z);
+  const b = a.backgroundColor;
+  const element = document.getElementById('product-image');
 
-
-
-
+  if (b === "rgb(0, 0, 0)") {
+    element.style.stroke="#fff";
+    element.style.fill=b;
+    } else {
+    element.style.stroke="#000";
+    element.style.fill=b;
+    }
+  }
