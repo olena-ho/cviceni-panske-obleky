6e9cebc461876c3e1d6dab2dd2b15cf6f6be1e58
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-04-05 15:24:54 +0200

    js for color changing

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // Color of t-shirt
 
 
+const svgElement = document.getElementById('product-image');
+const colorButtons = document.querySelectorAll('.colors__button');
+
+
+function changeSvgColor(event) {
+  
+  const color = event.target.style.backgroundColor;
+  svgElement.style.fill = color;
+}
+
+colorButtons.forEach(button => {
+  button.addEventListener('click', changeSvgColor);
+});
 
 
